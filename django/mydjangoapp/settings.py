f6e575667f9580b67cddb6d679ba09cc9f56a37c
gunji takeru <>
2019-11-17 14:33:35 +0900

    settings.py code 若干修正

--- django/mydjangoapp/settings.py
+++ django/mydjangoapp/settings.py
@@ -156,5 +156,5 @@ SITE_ID = 1
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
-    "allauth.account.auth_backends.AuthenticationBackend",
+    'allauth.account.auth_backends.AuthenticationBackend',
 )
